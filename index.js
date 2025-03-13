64be5b142c442db2e05595f1f0e9bf2959e84d7c
Gilbert Icheboi <gilbert.icheboi@student.moringaschool>
2025-03-13 06:02:33 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function superbowlWin(record) {
+    // Find the first record where the result is "W" and return the year.
+    const winRecord = record.find(r => r.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+
+  const sadReality = [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+expect(superbowlWin(sadReality)).to.equal(undefined)
+console.log(sadReality)
+  
+
+
+
+
+
